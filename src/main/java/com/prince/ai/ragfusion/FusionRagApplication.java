9794b7b40d07a31ff738e7b9f5d90ab9907897e6
Prince_Agrawal <prince.agrawal@tvh.com>
2026-06-29 23:43:36 +0530

    first commit

---
+++ src/main/java/com/prince/ai/ragfusion/FusionRagApplication.java
@@ -0,0 +1,13 @@
+package com.prince.ai.ragfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionRagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prince/ai/ragfusion/FusionRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prince.ai.ragfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusionRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
